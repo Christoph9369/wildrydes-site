be9b78b84c4ace1147f4d3876dc15b037083b2af
Christoph9369 <136021689+Christoph9369@users.noreply.github.com>
2025-03-06 14:57:25 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_SnGPSZxct', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4cu6lfa45104gosk522niavs5m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_HKiag6B4n', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ss3h2p80fvcpe19vcq31mm8dh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
